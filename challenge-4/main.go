2b9170ebbd90ce202ca30f938cb7f786db1bdb6c
bimaaryw <60120192+bimaaryw@users.noreply.github.com>
2023-03-17 00:46:08 +0700

    menghapus variabel

--- challenge-4/main.go
+++ challenge-4/main.go
@@ -22,33 +22,6 @@ func main(){
 //	fmt.Println(absen.Pekerjaan)
 //	fmt.Println(absen.Alasan)
 
-absen := map[int]map[string]string{
-    1: {
-        "nama":      "Mochamad",
-        "alamat":    "Jl.Jombang-Mojokerto No.1",
-        "pekerjaan": "Programmer",
-        "alasan":    "Ingin belajar lebih banyak tentang Go",
-    },
-    2: {
-        "nama":      "Syahrul",
-        "alamat":    "Jl.Jombang-Mojokerto No.2",
-        "pekerjaan": "Freelancer",
-        "alasan":    "Ingin tau tentang microservice",
-    },
-    3: {
-        "nama":      "Samsudin",
-        "alamat":    "Jl.Jombang-Mojokerto No.3",
-        "pekerjaan": "Karyawan",
-        "alasan":    "Mencari nafkah",
-    },
-    4: {
-        "nama":      "Hayolo",
-        "alamat":    "Jl.Jombang-Mojokerto No.4",
-        "pekerjaan": "Mahasiswa",
-        "alasan":    "Mencari ijazah",
-    },
-} 
-
 fmt.Println(absen.Nama)
 fmt.Println(absen.Alamat)
 fmt.Println(absen.Pekerjaan)
