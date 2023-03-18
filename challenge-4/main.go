4e9a95836cd408ef28be15d9507db1accad22438
bimaaryw <60120192+bimaaryw@users.noreply.github.com>
2023-03-18 21:12:42 +0700

    menambahkan struct

--- challenge-4/main.go
+++ challenge-4/main.go
@@ -10,11 +10,12 @@ func main(){
 	
 	//var absen1 = Absen{Nama: "Bima", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Sertifikat"}
 	//var absen2 = Absen{Nama: "Ary", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Mencari Ilmu"}
+	//var absen3 = Absen{Nama: "Widiatmaja", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Mencari teman baru"}
 
 	var absen = []Absen{
 		{Nama: "Bima", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Sertifikat"},
 		{Nama: "Ary", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Mencari Ilmu"},
-		{Nama: "Bima", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Sertifikat"},
+		{Nama: "Widiatmaja", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Mencari teman baru"},
 	}
 
 	//fmt.Println(absen1)
