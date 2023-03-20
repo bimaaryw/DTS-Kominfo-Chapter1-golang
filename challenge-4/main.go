e46d748a89485ed85355c55f72d8d2caf98ccbdd
bimaaryw <60120192+bimaaryw@users.noreply.github.com>
2023-03-20 22:49:54 +0700

    menambahkan os.Args

--- challenge-4/main.go
+++ challenge-4/main.go
@@ -1,6 +1,8 @@
 package main
 
 import "fmt"
+import "os"
+import "strconv"
 
 type Absen struct {
 	Nama,Alamat,Pekerjaan,Alasan	string
@@ -8,22 +10,25 @@ type Absen struct {
 
 func main(){
 	
-	//var absen1 = Absen{Nama: "Bima", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Sertifikat"}
-	//var absen2 = Absen{Nama: "Ary", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Mencari Ilmu"}
-	//var absen3 = Absen{Nama: "Widiatmaja", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Mencari teman baru"}
-
 	var absen = []Absen{
 		{Nama: "Bima", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Sertifikat"},
 		{Nama: "Ary", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Mencari Ilmu"},
 		{Nama: "Widiatmaja", Alamat: "Pati", Pekerjaan: "Mahasiswa", Alasan: "Mencari teman baru"},
 	}
 
-	//fmt.Println(absen1)
+	//fmt.Println(absen[0])
 	//fmt.Println(absen2)
 	
-	for  _, v := range absen {
-		fmt.Printf("%v \n", v)
-	}
+	//for  _, v := range absen {
+	//	fmt.Printf("%v \n", v)
+	//}
+
+	arg, _ := strconv.Atoi(os.Args[1])
+
+	fmt.Println("Nama : ", absen[arg].Nama)
+	fmt.Println("Alamat : ", absen[arg].Alamat)
+	fmt.Println("Pekerjaan : ", absen[arg].Pekerjaan)
+	fmt.Println("Alasan : ", absen[arg].Alasan)
 
 
 
