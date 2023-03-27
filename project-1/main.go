65854c987949ee42d68549f94391a69058a5e68a
bimaaryw <60120192+bimaaryw@users.noreply.github.com>
2023-03-27 21:23:07 +0700

    menambahkan progreess project 1

--- project-1/main.go
+++ project-1/main.go
@@ -1,11 +1,40 @@
 package main
 
-
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
 func main() {
-	go goroutine()
+
+	var data1 interface{} = []string{
+		"bisa 1",
+		"bisa 2",
+		"bisa 3",
+	}
+
+	var data2 interface{} = []string{
+		"coba 1",
+		"coba 2",
+		"coba 3",
+	}
+
+	runtime.GOMAXPROCS(2)
+
+	var wg sync.WaitGroup
+	for i := 1; i <= 8; i++ {
+		wg.Add(1)
+		go func(id int) {
+			if id%2 == 1 {
+				fmt.Printf("%v %d\n", data1, id)
+			} else {
+				fmt.Printf("%v %d\n", data2, id)
+			}
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
 }
 
-func goroutine() {
-
-}
+// blm selese msh proses tpi sya kumpulkan dlu
